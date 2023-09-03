03dc94d6a4d6967b633bdfca2892514abe302727
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-09-03 16:39:33 +0200

    prevent context menu on right click

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import runner from "./core/runner"
 
+// Add ObjectConstructor TypeScript typings for Object.keys and Object.values
+
 declare global {
   interface ObjectConstructor {
     keys<T>(object: T): (keyof T)[]
   }
 }
+
+// Disable context menu by right click
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault()
+})
 
 runner.start()
