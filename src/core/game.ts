a0ba66803588e5f4b8526beccd8fa8d5efbfc970
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-09-04 14:11:02 +0200

    updated booyah & added DebugComposite chip (WIP)

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,11 +1,13 @@
 import * as booyah from "@ghom/booyah"
 
 import * as fixtures from "../fixtures"
+import * as params from "../params"
 
 import app from "./app"
 
 import Fight from "../entities/Fight"
 import GridEditor from "../entities/GridEditor"
+import DebugComposite from "../debug/DebugComposite"
 
 class Game extends booyah.Composite {
   get defaultChildChipContext() {
@@ -15,11 +17,29 @@ class Game extends booyah.Composite {
   }
 
   protected _onActivate() {
+    if (params.debug) {
+      this._activateChildChip(
+        new DebugComposite(
+          this,
+          (log) => {
+            console.log(log)
+          },
+          [],
+          (name) =>
+            name === "Sequence" ||
+            name === "Parallel" ||
+            name === "StateMachine",
+        ),
+      )
+    }
+
     // this._activateChildChip(
     //   new Fight([fixtures.makeTeam(), fixtures.makeTeam()]),
     // )
 
-    this._activateChildChip(new GridEditor())
+    requestAnimationFrame(() => {
+      this._activateChildChip(new GridEditor())
+    })
   }
 
   protected _onTerminate() {}
---
+++ src/debug/DebugComposite.ts
@@ -0,0 +1,63 @@
+import * as booyah from "@ghom/booyah"
+
+export default class DebugComposite extends booyah.Composite {
+  constructor(
+    public readonly target: booyah.Chip,
+    private _onLog: (log: string) => void,
+    private _path: string[] = [],
+    /**
+     * Returns true if the path should be ignored.
+     */
+    private _ignore?: (name: string) => boolean,
+  ) {
+    super()
+  }
+
+  protected _onActivate() {
+    const path = this._path.slice(0)
+    const name = this.target.constructor.name
+
+    if (this._ignore && this._ignore(name)) {
+      this.terminate()
+      return
+    }
+
+    path.push(name)
+
+    this._subscribe(this.target, "activated", () =>
+      this._onLog(`${path.join(".")}: ON`),
+    )
+    this._subscribe(this.target, "terminated", () =>
+      this._onLog(`${path.join(".")}: OFF`),
+    )
+
+    if (
+      this.target instanceof booyah.Composite &&
+      !(this.target instanceof DebugComposite)
+    ) {
+      const debugList: DebugComposite[] = []
+
+      this._subscribe(this.target, "beforeActivatedChildChip", (child) => {
+        if (child instanceof DebugComposite) return
+
+        const debug = new DebugComposite(child, this._onLog, path)
+
+        debugList.push(debug)
+
+        this._activateChildChip(debug)
+      })
+
+      this._subscribe(this.target, "terminatedChildChip", (child) => {
+        if (child instanceof DebugComposite) return
+
+        const debug = debugList.find((debug) => debug.target === child)
+
+        if (debug) {
+          this._terminateChildChip(debug)
+
+          debugList.splice(debugList.indexOf(debug), 1)
+        }
+      })
+    }
+  }
+}
